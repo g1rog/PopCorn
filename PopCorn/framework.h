76463b0a322c7cb6b6472346ba5c42092e59cadf
g1rog <sunnycovex@gmail.com>
2022-12-02 23:04:52 +0700

    WinAPI template

---
+++ PopCorn/framework.h
@@ -0,0 +1,10 @@
+
+#pragma once
+#define WIN32_LEAN_AND_MEAN           
+
+#include "targetver.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
---
+++ PopCorn/main.cpp
@@ -0,0 +1,138 @@
+
+#include "framework.h"
+#include "main.h"
+
+static constexpr auto MAX_LOADSTRING = 100;
+
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];                 
+WCHAR szWindowClass[MAX_LOADSTRING];           
+ATOM MyRegisterClass(const HINSTANCE& hInstance);
+BOOL InitInstance(const HINSTANCE&, const int);
+
+constexpr LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+constexpr INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, 
+    _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_POPCORN, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance (hInstance, nCmdShow)) return FALSE; 
+    const HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_POPCORN));
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    return static_cast<int>(msg.wParam);
+}
+
+ATOM MyRegisterClass(const HINSTANCE& hInstance)
+{
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_POPCORN));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = CreateSolidBrush(RGB(0,0,0));
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_POPCORN);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(const HINSTANCE& hInstance, const int nCmdShow)
+{
+    RECT window_rect;
+    window_rect.left = 0;
+    window_rect.top = 0;
+    window_rect.right = 320 * 3;
+    window_rect.bottom = 200 * 3;
+
+   AdjustWindowRect(&window_rect, WS_OVERLAPPEDWINDOW, TRUE);
+   hInst = hInstance; 
+   const HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+     0, 0, 320 * 3, 200 * 3, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd) return FALSE;  
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+consteval void Draw_Frame(const HDC& hdc)
+{
+    
+}
+
+constexpr LRESULT CALLBACK WndProc(const HWND hWnd, const UINT message, const WPARAM wParam, const LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            switch (const auto wmId = LOWORD(wParam))
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            const HDC hdc = BeginPaint(hWnd, &ps);
+            Draw_Frame(hdc);
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+constexpr INT_PTR CALLBACK About(const HWND hDlg, const UINT message, const WPARAM wParam, const LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return static_cast<INT_PTR>(TRUE);
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return static_cast<INT_PTR>(TRUE);
+        }
+        break;
+    }
+    return static_cast<INT_PTR>(FALSE);
+}
